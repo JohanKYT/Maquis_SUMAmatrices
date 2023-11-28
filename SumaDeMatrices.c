3a304fd83e9b8786e053d51faafa9c854c30de16
Kevin Maquis <kevin.maquis@udla.edu.ec>
2023-11-28 15:24:32 -0500

    iniciamos la funcion main y definimos variables ademas de que ponemos las dimenciones que meta el usuario

--- SumaDeMatrices.c
+++ SumaDeMatrices.c
@@ -2,3 +2,23 @@
 #include <stdlib.h>
 #include <time.h>
 // Kevin Calle Maquis Calle
+int main()
+{
+    // Agregamos la funcion para que no nos de error al usar una funcion que nos brinde valores aleatorios
+    srand(time(NULL));
+    int n, m, o, p;
+    // Inicializamos las variables y ponemos que el usuario meta los valores del tama;o de la matriz y leemos eso
+    printf("Ingeres el numero de filas MATRIZ 1:\n");
+    scanf("%d", &n);
+    printf("Ingeres el numero de columnas MATRIZ 1:\n");
+    scanf("%d", &m);
+    // Inicializamos las variables y ponemos que el usuario meta los valores del tama;o de la matriz y leemos eso
+    printf("Ingeres el numero de filas MATRIZ 2:\n");
+    scanf("%d", &o);
+    printf("Ingeres el numero de columnas MATRIZ 2:\n");
+    scanf("%d", &p);
+
+    
+
+    return 0;
+}
